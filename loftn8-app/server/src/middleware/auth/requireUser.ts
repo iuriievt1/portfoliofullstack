2fbca4f7a0ad0ea8d346d826a482b7d6bbc615b9
iuriievt <iuriievt@gmail.com>
2026-03-21 20:34:13 +0100

    Delete loftn8-app directory

--- loftn8-app/server/src/middleware/auth/requireUser.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response, NextFunction } from "express";
-import { HttpError } from "../../utils/httpError";
-
-export function requireUser(req: Request, _res: Response, next: NextFunction) {
-  if (!req.user) {
-    return next(new HttpError(403, "AUTH_REQUIRED", "User authentication required"));
-  }
-  next();
-}

69da8cee0523c3cbed7f55c7bfcca5672e235c68
iuriievt <iuriievt@gmail.com>
2026-03-21 19:46:45 +0100

    Delete loftn8-app directory

--- loftn8-app/client/src/app/staff/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { StaffSessionProvider } from "@/providers/staffSession";
-
-export default function StaffRootLayout({ children }: { children: React.ReactNode }) {
-  return <StaffSessionProvider>{children}</StaffSessionProvider>;
-}
--- loftn8-app/server/prisma/migrations/20260209172602_menu_sections/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "MenuSection" AS ENUM ('KITCHEN', 'BAR', 'HOOKAH');
-
--- AlterTable
-ALTER TABLE "MenuCategory" ADD COLUMN     "section" "MenuSection" NOT NULL DEFAULT 'KITCHEN';
